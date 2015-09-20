a413586f11d0224d1d8b366aa2dddfcd85a68171
williamthing <williamthing@gmail.com>
2015-09-19 22:56:49 -0700

    fixed stock calc algorithm had errors prior

--- Python/calc-annual-div.py
+++ Python/calc-annual-div.py
@@ -14,17 +14,17 @@ annual_div = []
 
 for row in input_file:
     share_val = float(row["share_val"])
-    compound_div = float(row["div_yield"]) * 4 * share_val
+    num_shares = float(row["num_shares"])
+    compound_div = float(row["div_yield"]) * 4 * num_shares
     annual_div.append(compound_div)
     total += share_val
 
 # calc dividend percentage
 # calc annual dividend yield
-annual_div_yield = 0.00
+annual_div_yield = 0.00     # annual div yield is in percentage
 for div in annual_div:
     annual_div_yield += div / total
 
-annual_div_yield = annual_div_yield / total * 100
 print "-----Portfolio Statistics-----"
 print 'Annual Dividend Yield: {0:.2f}%'.format(annual_div_yield * 100)
 print 'Annual Payout: ${0:.2f}'.format(annual_div_yield * total)
